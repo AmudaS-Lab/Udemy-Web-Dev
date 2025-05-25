561c1064ec5fa5bd4a7a27e64fe5d3c423ad0212
AmudaS-Lab <amuda183@gmail.com>
2025-05-25 10:22:58 +0530

    20/05/2025

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,14 @@
+// // var generateName = require("sillyname");
+// import generateName from "sillyName";
+
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+// const superheroes = require("superheroes");
+// // import superheroes from "superheroes";
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+
+// const name = superheroes.randomSuperhero();
+console.log(`I am ${name}!`);
